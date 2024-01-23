3f00a07d62035738915a68368594ad1cd796af37
Xaquzy <143174417+Xaquzy@users.noreply.github.com>
2024-01-23 12:40:18 +0100

    Trying to drop

---
+++ Assets/Scripts/PickupAndDrop.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class PickupAndDrop : MonoBehaviour
+{
+    [SerializeField] private Transform lookDir;
+    [SerializeField] private Transform objectGrabPointTransform;
+    [SerializeField] float pickUpDistance = 2.0f;
+    [SerializeField] private LayerMask pickUpLayerMask;
+
+    private ObjectGrabble objectGrabble;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (objectGrabble == null)
+            {
+                //Not carrying an object, try to grab
+                if (Physics.Raycast(lookDir.position, lookDir.forward, out RaycastHit raycastHit, pickUpDistance, pickUpLayerMask))
+                {
+                    if (raycastHit.transform.TryGetComponent(out ObjectGrabble objectGrabble))
+                    {
+                        objectGrabble.Grab(objectGrabPointTransform);
+                    }
+                }
+                else
+                {
+                    UnityEngine.Debug.Log("Cannot grab this object");
+                }
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            // Currently carrying something, drop it
+            if (objectGrabble != null)
+            {
+                objectGrabble.Drop();
+                objectGrabble = null;
+            }
+        }
+    }
+}
