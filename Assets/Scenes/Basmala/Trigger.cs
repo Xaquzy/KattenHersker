534f35bf03118e43b8fc5183ad29d9a7a8160174
Xaquzy <143174417+Xaquzy@users.noreply.github.com>
2024-01-23 14:32:15 +0100

    øhmm

--- Assets/Scenes/Basmala/Trigger.cs
+++ Assets/Scenes/Basmala/Trigger.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class Trigger : MonoBehaviour
 {
 
-    Tool
+
     // Start is called before the first frame update
     void Start()
     {
-
-        
+            
     }
 
     // Update is called once per frame
