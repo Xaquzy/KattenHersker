3399e459de6bf9cf0cb91abeb406d4616f5fcbc6
byggemandb <basmalayj@hotmail.com>
2024-02-02 14:26:09 +0100

    Textbox?

--- Assets/Scripts/CollectingAnimals.cs
+++ Assets/Scripts/CollectingAnimals.cs
@@ -15,16 +15,12 @@ public class CollectingAnimals : MonoBehaviour
         {
             // Tilføj et tilfældigt tal til listen, der repræsenterer det samlede dyr
             Animals.Add(Random.Range(1, 10));
-            Debug.Log("Dyr samlet op! Tilføjede et tal til listen. Antal elementer i listen: " + Animals.Count);
+            Debug.Log("Dyret følger dig! Tilføjede et tal til listen. Antal elementer i listen: " + Animals.Count);
 
             // Tjek om der er tre elementer i listen
             if (Animals.Count == 3)
             {
-                // Gør noget når der er tre elementer i listen
-                //DoSomething();
-
-                // Ryd listen
-                //minListe.Clear();
+                //Load                
             }
         }
     }
