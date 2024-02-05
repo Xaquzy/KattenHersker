678297e554412f038fc5da506ac5c51b23867512
Xaquzy <143174417+Xaquzy@users.noreply.github.com>
2024-02-05 19:48:54 +0100

    Overordnet Kohærens i prototypen

--- Assets/Scripts/FreeLookCutSceneAndChange.cs
+++ Assets/Scripts/FreeLookCutSceneAndChange.cs
@@ -6,9 +6,10 @@ using UnityEngine.SceneManagement;
 public class FreeLookCutSceneAndChange : MonoBehaviour
 {
 
-    public GameObject player;
-    public GameObject key;
+    //public GameObject player;
+    //public GameObject key;
     public CinemachineFreeLook cutsceneCam;
+    public CinemachineFreeLook MainCam;
     [SerializeField] private float CutSceneTime = 3f;
     [SerializeField] private int NewSceneNumber = 1;
     [SerializeField] AudioSource dialog;
@@ -17,25 +18,23 @@ public class FreeLookCutSceneAndChange : MonoBehaviour
     {
         if (other.CompareTag("PlayerTrigger"))
         {
-            {
-                Debug.Log("YAY ITS WORKING LILILILILILILILILILILILI");
-                this.gameObject.GetComponent<BoxCollider>().enabled = false;
-                cutsceneCam.enabled = true;
-                player.SetActive(false);
-                key.SetActive(false);
-                //Disable mouse camera input
-                //Play animation of the cutscene
-                dialog.Play();
-                StartCoroutine(FinishCut());
-            }
+            Debug.Log("YAY ITS WORKING LILILILILILILILILILILILI");
+            this.gameObject.GetComponent<BoxCollider>().enabled = false;
+            cutsceneCam.enabled = true;
+            MainCam.enabled = false;
+            //player.SetActive(false);
+            //key.SetActive(false);
+            //Disable mouse camera input
+            //Play animation of the cutscene
+            dialog.Play();
+            StartCoroutine(FinishCut());
+        }
 
-            //Scenechange after 10s of cut scene
-            IEnumerator FinishCut()
-            {
-                yield return new WaitForSeconds(CutSceneTime);
-                SceneManager.LoadScene(NewSceneNumber);
-            }
-
+        //Scenechange after 10s of cut scene
+        IEnumerator FinishCut()
+        {
+            yield return new WaitForSeconds(CutSceneTime);
+            SceneManager.LoadScene(NewSceneNumber);
         }
     }
 }
