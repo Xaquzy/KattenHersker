6ba3c2b5a045e70041b9c8c3588b8d4203328948
byggemandb <basmalayj@hotmail.com>
2024-02-19 16:56:28 +0100

    lalalala

--- Assets/Scripts/JailBreakScene.cs
+++ Assets/Scripts/JailBreakScene.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class CutsceneAndSceneChange : MonoBehaviour
+public class JailBreakScene : MonoBehaviour
 {
     public Transform JaildoorTrigger;
     public Camera cutsceneCam;
